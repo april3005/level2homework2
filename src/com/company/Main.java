13beeae9e7644eec21e09cc441add14232fe2c3a
april3005 <55992572+april3005@users.noreply.github.com>
2019-11-11 23:29:24 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Clothes clothes = createObject("Dress");
+        System.out.println(clothes.getName());
+    }
 
-        public createObject (String className),
-            return
-
+    public static Clothes createObject(String objectName) {
+        switch (objectName) {
+            case "Dress":
+                return new Dress("Sample", "Silk", 4, "No pattern");
+            case "Jeans":
+                return new Jeans("Levis", "Cotton", 6, "Mom jeans");
+            case "Shirt":
+                return new Shirt("Madelene", "Silk", 4, "Yellow");
+            default:
+                return null;
         }
     }
 }
